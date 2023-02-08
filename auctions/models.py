eb178e60baa7c58cdcf09e5d22ad6e4537d09c5c
Tarvalas <jamesdwyer1@live.com>
2023-02-08 11:40:48 -0500

    added initial model for listing and some related test cases

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=80, blank=False)
+    description = models.TextField(max_length=800)
+    start_bid = models.DecimalField(max_digits=12, decimal_places=2, validators=[MinValueValidator(0.00)])
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
     pass
--- auctions/tests.py
+++ auctions/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import authenticate
-from .models import User
+from django.core.exceptions import ValidationError
+from .models import User, Listing, Bid, Comment
 
 # Create your tests here.
-class UserTestCase(TestCase):
+class UserModelTestCase(TestCase):
     def setUp(self):
-        user = User.objects.create_user(username="test", password="12345")
+        User.objects.create_user(username="test", password="12345")
 
     def test_correct_user(self):
         user = authenticate(username="test", password="12345")
@@ -18,3 +19,25 @@ class UserTestCase(TestCase):
     def test_incorrect_password(self):
         user = authenticate(username="test", password="fail")
         self.assertFalse(user is not None and user.is_authenticated)
+
+
+class ListModelTestCase(TestCase):
+    def setUp(self):
+        listing = Listing.objects.create(title="Testing", description="description", start_bid=10.00)
+        listing.save()
+
+    def test_correct_listing(self):
+        listing = Listing.objects.get()
+        self.assertTrue(listing is not None)
+
+    def test_incorrect_listing_title(self):
+        listing = Listing.objects.create(title="", description="description", start_bid=10.00)
+        self.assertRaises(ValidationError, listing.full_clean)
+
+    def test_incorrect_listing_bid_negative(self):
+        listing = Listing.objects.create(title="Testing", description="description", start_bid=-10.00)
+        self.assertRaises(ValidationError, listing.full_clean)
+
+    def test_incorrect_listing_bid_digits(self):
+        listing = Listing.objects.create(title="Testing", description="description", start_bid=10.001)
+        self.assertRaises(ValidationError, listing.full_clean)
