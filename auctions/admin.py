8cefaa87d2184f619f0af5414a9cab766c41942c
Tarvalas <jamesdwyer1@live.com>
2023-02-09 15:27:11 -0500

    further adjusted Listing model and also admin panel

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Listing, Bid, Comment
+
+class ListingInline(admin.TabularInline):
+    model = Listing
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [ListingInline] 
+
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- auctions/models.py
+++ auctions/models.py
@@ -7,17 +7,26 @@ from taggit.managers import TaggableManager
 class User(AbstractUser):
     pass
 
+    def __str__(self):
+        return f"{self.username}"
+
+
 class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=80, blank=False)
     description = models.TextField(max_length=800)
     start_bid = models.DecimalField(max_digits=12, decimal_places=2, validators=[MinValueValidator(0.00)])
-    image_url = models.URLField()
-    tags = TaggableManager()
-    
+    image_url = models.URLField(blank=True)
+    tags = TaggableManager(blank=True)
+    list_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.title}"
+    
 
 class Bid(models.Model):
     pass
+
 
 class Comment(models.Model):
     pass
